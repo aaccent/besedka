1856a2976ef44c7d3faaf468109bcdc66ae7696e
Regina <rmyachina@mail.ru>
2024-02-27 18:18:07 +0300

    init

---
+++ postcss.config.cjs
@@ -0,0 +1,19 @@
+const autoScales = require('./postcss/auto-scales.cjs')
+
+module.exports = {
+  plugins: [
+    autoScales({
+      dodgeSelectors: [
+        { type: 'startsWith', value: '.swiper' },
+        { type: 'startsWith', value: '.carousel' },
+        { type: 'includes', value: 'fancybox' },
+        { type: 'includes', value: 'aos' },
+      ],
+      limitMediaWidth: false,
+      initWidth: [
+        { width: 1728 },
+        { width: 390, mediaQuery: 'only screen and (max-width: 1000px)' },
+      ],
+    }),
+  ],
+}
