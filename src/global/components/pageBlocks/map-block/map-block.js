8a22425039c8ff20d96512d2449c0f341e291043
Regina <rmyachina@mail.ru>
2024-03-20 11:18:17 +0300

    issues

--- src/global/components/pageBlocks/map-block/map-block.js
+++ src/global/components/pageBlocks/map-block/map-block.js
@@ -8,6 +8,7 @@ void function() {
     const lng = Number(contact.dataset.lng)
     const lat = Number(contact.dataset.lat)
     coords.push({ lng, lat })
+    console.log(coords)
   })
 
   function initMarkers(map) {
