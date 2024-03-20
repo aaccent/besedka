6cd762083dd945f20b5c98aff92824cac06dcf94
Regina <rmyachina@mail.ru>
2024-03-20 18:01:59 +0300

    feature/animation

--- src/pages/index/sections/info/info.js
+++ src/pages/index/sections/info/info.js
@@ -1,28 +1,22 @@
 import { gsap } from "gsap";
 import { Flip } from "gsap/Flip.js";
+
 gsap.registerPlugin(Flip);
-
-
-
 
 setInterval(() => {
     const first = document.querySelectorAll('.first');
     const second = document.querySelectorAll('.second');
     const third = document.querySelectorAll('.third');
 
-    const state = Flip.getState(document.querySelectorAll('.info__block-img'), { props: 'top, left, translate, height' })
-
-    first.forEach(el => el.classList.replace('first', 'noHeight'));
+    first.forEach(el => {
+        el.classList.add('down');
+        setTimeout(() => {
+            el.classList.replace('first', 'third');
+            el.classList.remove('down');
+        }, 800)
+    });
 
     second.forEach(el => el.classList.replace('second', 'first'));
 
     third.forEach(el => el.classList.replace('third', 'second'));
-
-    first.forEach(el => el.classList.replace('noHeight', 'third'));
-
-    Flip.from(state, {
-        duration: 1,
-        ease: "power1.inOut",
-        scale: true,
-    })
-}, 4000)
+ }, 4000)
